fc0535628f6afe4f555755f223e68c48ab85c7c7
user <user@localhost.localdomain>
2017-02-18 01:34:34 +0300

    Создал простую сессию #7

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, session, url_for, escape, request, Flask
 from app import app, chats
 from forms import LoginForm
-
+from beaker.middleware import SessionMiddleware
 
 @app.route('/')
 def index():
@@ -14,8 +14,17 @@ def index():
 def login_page():
     form = LoginForm()
     if form.validate_on_submit():
+        #if request.method == 'POST':
+        session['username'] = request.form
+        return redirect('/chat/1')
+    if 'username' in session :
         return redirect('/chat/1')
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
 @app.route('/chat/<int:chat_id>', methods=['GET', 'POST'])
@@ -42,3 +51,5 @@ def get_messages():
         return '<br>'.join(map(lambda x: x[0] + ' ' + x[1], chats[chat_id].messages[index:]))
     except BaseException:
         return 'Error'
+
+app.secret_key = 'OMG_so_secret_SH1T'

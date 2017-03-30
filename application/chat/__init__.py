7c14fd75fb16ee96e97283032d0d5d12a25a7269
vados <big-vadim@mail.ru>
2017-03-30 20:30:22 +0300

    табы заменены пробелами

--- application/chat/__init__.py
+++ application/chat/__init__.py
@@ -22,13 +22,13 @@ def get_messages(id, index):
     ret = []
     for i in result:
 
-    	if i.type == "usr":
-    		if i.author == session['login']:
-    			type = "mine"
-    		else:
-    			type = "others"
-    	else:
-    		type = "system"
+        if i.type == "usr":
+            if i.author == session['login']:
+                type = "mine"
+            else:
+                type = "others"
+        else:
+            type = "system"
 
         ret.append({"author": i.author, "message": i.content, "type": type})
     return ret

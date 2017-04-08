3a92c943fb98f5853338b75d1bd782fffa8927ea
Shirokov <you@abc.net>
2017-04-08 19:13:05 +0300

    31 Markdown lib

--- application/chat/__init__.py
+++ application/chat/__init__.py
@@ -2,15 +2,11 @@ from flask import session
 from application.models import Message, Code, Chat
 from application import db, socketio
 from flask_socketio import emit
+from markdown import Markdown
 
-
-def html_special_chars(text):
-    return text \
-    .replace(u"&", u"&amp;") \
-    .replace(u'"', u"&quot;") \
-    .replace(u"'", u"&#039;") \
-    .replace(u"<", u"&lt;") \
-    .replace(u">", u"&gt;")
+def markdown(text):
+    md = Markdown(str(text))
+    return md.toString()
 
 def create_chat(name, code, username):
     chat_to_create = Chat(name)
@@ -25,6 +21,7 @@ def get_chat_info(id):
     return {'name':result.name}
 
 def send_message(id, text, type, username):
+    text = markdown(text)
     db.session.add(Message(text, username, id, type))
     db.session.commit()
 

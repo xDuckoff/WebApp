1cf8eaebe0a2cbfab640c3d1d2979bc543648794
Maxim Gaponov <gaponovmaxev@gmail.com>
2017-05-01 14:54:37 +0300

    ограничил длину сообщения

--- application/chat/router.py
+++ application/chat/router.py
@@ -13,6 +13,8 @@ if app.config['SOCKET_MODE'] == 'True':
     @socketio.on('message')
     def handle_message(json):
         chat_id = int(json['room'])
+        if len(json['message']) > 1000:
+            return
         chat.send_message(chat_id, json['message'], 'usr', session['login'])
         socketio.emit('message', {'message':json['message'], 'author':session['login'], 'type':'usr'}, json=True, room=json['room'], broadcast=True)
 
@@ -60,6 +62,8 @@ if app.config['SOCKET_MODE'] == 'False':
             return redirect('/login')
         chat_id = int(request.args['chat'])
         message = request.args['message']
+        if len(message) > 1000:
+            return 'LENGTH LIMIT'
         if len(message) > 0:
             chat.send_message(chat_id, message, "usr", session['login'])
         return 'OK'

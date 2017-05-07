92d0bc7133177f85bd6be13759aaeee6221a163d
Maxim Gaponov <gaponovmaxev@gmail.com>
2017-05-07 23:22:41 +0300

    Прибрался в HTML. Удалил лишние файлы. Перенёс логин на главную. Удалил "подключиться к чату". (#71 #70)

--- application/router.py
+++ application/router.py
@@ -5,33 +5,23 @@ from application import app, chat
 from forms import LoginForm, login_user, IsInSession
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login_page():
-    form = LoginForm()
-    if 'chat' in request.args:
-        link = '/chat/' + request.args['chat']
-    else:
-        link = '/'
-    if form.validate_on_submit():
-        login_user(form.login.data)
-        return redirect(link)
-    return render_template('login.html', form=form)
-
-
 @app.route('/logout')
 def logout():
     session.pop('login', None)
     return redirect('/')    
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     chat_title = request.args.get('search_title_text', '')
     if chat_title == '':
         chats = []
     else:
         chats=chat.find_chat(str(chat_title))
-    return render_template('index.html', chats=chats, in_session=IsInSession())
+    form = LoginForm()
+    if form.validate_on_submit():
+        login_user(form.login.data)
+    return render_template('index.html', chats=chats, in_session=IsInSession(), form=form)
 
 
 import application.chat.router

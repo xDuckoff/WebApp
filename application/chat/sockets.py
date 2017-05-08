87df119821dab824cc54a1e975d7ab58921f4874
vados <big-vadim@mail.ru>
2017-05-08 23:50:54 +0300

    Услвие режима сокетов с импортом

--- application/chat/sockets.py
+++ application/chat/sockets.py
@@ -1,14 +1,15 @@
 from application import app
-if app.config['SOCKET_MODE'] == 'True':
-    
-    from flask_socketio import emit
+
+def check_mode():
+	if app.config['SOCKET_MODE'] == 'True':
+	    from application import socketio
+	    from flask_socketio import emit
+	return app.config['SOCKET_MODE'] == 'True'
 
 def send_code_sockets(id):
-    if app.config['SOCKET_MODE'] == 'True':
-    	from application import socketio
+    if check_mode():
         socketio.emit('commit', room=str(id), broadcast=True)
 
 def sys_message_sockets(data, room):
-    if app.config['SOCKET_MODE'] == 'True':
-    	from application import socketio
+    if check_mode():
         socketio.emit('message', {'message':data, 'author':'System', 'type':'sys'}, room=room, broadcast=True)

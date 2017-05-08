27469a7a41ede1e24f7c89847286394e8e43b9df
vados <big-vadim@mail.ru>
2017-05-09 00:24:16 +0300

    Тесты работают без сокетов

--- application/chat/sockets.py
+++ application/chat/sockets.py
@@ -5,6 +5,7 @@ socketio = 0
 def check_mode():
 	if app.config['SOCKET_MODE'] == 'True':
 		from flask_socketio import SocketIO
+		global socketio
 		socketio = SocketIO(app)
 		from flask_socketio import emit
 		from sockets_routers import create_routers
@@ -13,8 +14,10 @@ def check_mode():
 
 def send_code_sockets(id):
     if check_mode():
+    	global socketio
         socketio.emit('commit', room=str(id), broadcast=True)
 
 def sys_message_sockets(data, room):
     if check_mode():
+    	global socketio
         socketio.emit('message', {'message':data, 'author':'System', 'type':'sys'}, room=room, broadcast=True)

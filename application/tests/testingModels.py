ca83a1ed29fcca20b15361a0fadc3dfa4db8bc4f
Dubinskiy_Egor <deydarachan@gmail.com>
2017-05-13 19:32:32 +0300

    тестирование моделей

--- application/tests/testingModels.py
+++ application/tests/testingModels.py
@@ -19,19 +19,40 @@ class Testmodels(unittest.TestCase):
             flask_migrate.upgrade()
 
 
+
+
     def tearDown(self):
         os.remove(PATH_TO_DATABASE)
 
-    
+    def test_available_chat(self):
+        chat = Chat("some")
+        self.assertTrue(hasattr(chat, "id"))
+        self.assertTrue(hasattr(chat, "name"))
 
-    def test_type_model_chat(self):
-        chat =Chat("some")
+    def test_available_message(self):
+        message = Message("content", "author", 1, "sys")
+        self.assertTrue(hasattr(message, "id"))
+        self.assertTrue(hasattr(message, "content"))
+        self.assertTrue(hasattr(message, "author"))
+        self.assertTrue(hasattr(message, "chat"))
+        self.assertTrue(hasattr(message, "type"))
+
+    def test_available_code(self):
+        code = Code("content", "author", 1, 0)
+        self.assertTrue(hasattr(code, "id"))
+        self.assertTrue(hasattr(code, "content"))
+        self.assertTrue(hasattr(code, "author"))
+        self.assertTrue(hasattr(code, "chat"))
+        self.assertTrue(hasattr(code, "parent"))
+
+    def test_type_model_message(self):
+
         self.assertIsInstance(Chat.id.type, db.Integer)
         self.assertIsInstance(Chat.name.type, db.String)
 
 
     def test_type_model_message(self):
-        message = Message("content", "author", 1, "sys")
+
         self.assertIsInstance(Message.id.type, db.Integer)
         self.assertIsInstance(Message.content.type, db.Text)
         self.assertIsInstance(Message.author.type, db.String)
@@ -40,7 +61,7 @@ class Testmodels(unittest.TestCase):
 
 
     def test_type_model_code(self):
-        code = Code("content", "author", 1, 0)
+
         self.assertIsInstance(Code.id.type, db.Integer)
         self.assertIsInstance(Code.content.type, db.Text)
         self.assertIsInstance(Code.author.type, db.String)

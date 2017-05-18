827b6edc5b6be21eef918a23fccfc44e2e5232f1
Maxim Sidorenko <mr.maximsms@gmail.com>
2017-05-18 12:34:41 -0700

    #81 Теперь можно просматривать все файлы в папке документации, в том числе и документацию

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 Данный файл содержит основные положения запуска приложения
 """
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('config', )
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 

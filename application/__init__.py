57dec40547979de790f23a0d91ea4e092a5d4bae
Maxim Sidorenko <mr.maximsms@gmail.com>
2017-05-25 11:32:24 -0700

    #74 Подключил config добавил исключением invalid-name

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
+"""
+Данный файл содержит основные положения запуска приложения
+"""
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 
-"""
-Данный файл содержит основные положения запуска приложения
-"""
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)

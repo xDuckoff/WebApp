a61b28eca915e90e19b47c7c486e2b0e5184fc28
MaGaponov <gaponovmaxev@gmail.com>
2017-06-07 11:31:22 +0300

    Auto-upgrade DB

--- application/__init__.py
+++ application/__init__.py
@@ -2,18 +2,21 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from flask_wtf.csrf import CSRFProtect
 
 """
 Данный файл содержит основные положения запуска приложения
 """
 
-csrf = CSRFProtect()
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+with app.app_context():
+    upgrade()
+csrf = CSRFProtect()
 csrf.init_app(app)
+
 
 import router

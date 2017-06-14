7175b816b91669caab1d7042d5007dfe7933317f
T.Vladimir <vovatatosyan2000@mail.ru>
2017-06-14 13:01:57 +0200

    перезаписал в нужный файлы команды для #17

--- run.py
+++ run.py
@@ -3,3 +3,15 @@
 """Запуск проекта"""
 
 from application import app
+
+import flask_migrate
+
+with app.app_context():    
+	flask_migrate.upgrade()
+
+	import os
+
+	go_to_dir = 'cd docs/'
+	make_html = 'make html'
+	go_up = 'cd ..'
+	os.system(go_to_dir + '\n' + make_html + '\n' + go_up + '\n')

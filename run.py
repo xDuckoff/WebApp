6d53c9bc8918848246e957d6f16951af1b57dbfe
T.Vladimir <vovatatosyan2000@mail.ru>
2017-06-14 13:20:11 +0200

    правильное оформление #17

--- run.py
+++ run.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """Запуск проекта"""
-
-from application import app
-
+import os
 import flask_migrate
+from application import app
 
 with app.app_context():    
 	flask_migrate.upgrade()
 
-	import os
-
 	go_to_dir = 'cd docs/'
 	make_html = 'make html'
 	go_up = 'cd ..'
+
 	os.system(go_to_dir + '\n' + make_html + '\n' + go_up + '\n')

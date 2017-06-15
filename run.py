61c6d9b0589c5542ef1bb7bff703dcd1a173b4ea
T.Vladimir <vovatatosyan2000@mail.ru>
2017-06-15 19:56:15 +0200

    #20 изменил табы на пробелы

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ import os
 import flask_migrate
 from application import app
 
-with app.app_context():    
-	flask_migrate.upgrade()
+with app.app_context():
+    flask_migrate.upgrade()
 
-	go_to_dir = 'cd docs/'
-	make_html = 'make html'
-	go_up = 'cd ..'
+    go_to_dir = 'cd docs/'
+    make_html = 'make html'
+    go_up = 'cd ..'
 
-	os.system(go_to_dir + '\n' + make_html + '\n' + go_up + '\n')
+    os.system(go_to_dir + '\n' + make_html + '\n' + go_up + '\n')

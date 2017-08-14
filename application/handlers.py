c433392c5e40b16f371e44ee02ab8076c47fd5ab
MaGaponov <gaponovmaxev@gmail.com>
2017-08-14 17:05:04 +0300

    #22 #23 Удалил csrf_required

--- application/handlers.py
+++ application/handlers.py
@@ -4,25 +4,9 @@
 
 from json import dumps
 from functools import wraps
-from wtforms.validators import ValidationError
 from application.models import User, Chat
 from application.forms import ChatForm
 from flask import request
-
-
-def csrf_required(func):
-    """Проверка csrf-ключа"""
-    @wraps(func)
-    def csrf_check(*args, **kwargs):
-        """Функция возвращает ошибку, если csrf-ключ невалиден"""
-        try:
-            User.check_csrf()
-        except ValidationError:
-            return dumps({"success": False, "error": "Security error"}), 403
-        except KeyError:
-            return dumps({"success": False, "error": "Security error"}), 403
-        return func(*args, **kwargs)
-    return csrf_check
 
 
 def access_required(func):

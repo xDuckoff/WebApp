2758593b184ba1fa9fcdb7b4cec875edcc0039a9
John Syomochkin <info@syomochkin.xyz>
2017-08-19 18:34:58 +0300

    удалил лишнюю зависимость flask_recaptcha, удалил тесты этой зависимости #20

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
 from flask_wtf.csrf import CSRFProtect
 from flask_socketio import SocketIO
-from flask_recaptcha import ReCaptcha
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -16,6 +15,5 @@ migrate = Migrate(app, db)
 csrf = CSRFProtect()
 csrf.init_app(app)
 socketio = SocketIO(app)
-recaptcha = ReCaptcha(app=app)
 
 import router

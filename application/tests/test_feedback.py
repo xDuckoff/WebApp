bc01d5bf005c50934c75e045337adce662fa8384
John Syomochkin <info@syomochkin.xyz>
2017-08-24 19:17:04 +0300

    обновил тесты, поправил документацию #21

--- application/tests/test_feedback.py
+++ application/tests/test_feedback.py
@@ -5,10 +5,12 @@
 from base_test_model import *
 from application import db
 from application.models import Feedback
+from mock import patch
 
 FEEDBACK_NAME = "test bot"
 FEEDBACK_EMAIL = "test@test.com"
 FEEDBACK_TEXT = "Lorem Ipsum"
+TRELLO_CARD_URL = "http://trello/card/id"
 
 
 class TestFeedbackModel(BaseTestModel):
@@ -24,16 +26,26 @@ class TestFeedbackModel(BaseTestModel):
         self.assertTrue(hasattr(feedback, "name"))
         self.assertTrue(hasattr(feedback, "email"))
         self.assertTrue(hasattr(feedback, "text"))
+        self.assertTrue(hasattr(feedback, "trello_link"))
 
     def test_type_model_feedback(self):
         self.assertIsInstance(Feedback.id.type, db.Integer)
         self.assertIsInstance(Feedback.name.type, db.String)
         self.assertIsInstance(Feedback.email.type, db.String)
         self.assertIsInstance(Feedback.text.type, db.Text)
+        self.assertIsInstance(Feedback.trello_link.type, db.String)
 
-    def test_feedback_sending(self):
+    @patch.object(Feedback, 'send_to_trello', return_value=None)
+    def test_feedback_sending(self, *args):
         send_feedback = Feedback.send(FEEDBACK_NAME, FEEDBACK_EMAIL, FEEDBACK_TEXT)
         feedback_in_db = Feedback.query.get(send_feedback.id)
         self.assertEquals(feedback_in_db.name, FEEDBACK_NAME)
         self.assertEquals(feedback_in_db.email, FEEDBACK_EMAIL)
         self.assertEquals(feedback_in_db.text, FEEDBACK_TEXT)
+
+    @patch.object(Feedback, 'send_to_trello', return_value=TRELLO_CARD_URL)
+    def test_trello_card_url_after_send_feedback(self, mock_send_to_trello_method):
+        send_feedback = Feedback.send(FEEDBACK_NAME, FEEDBACK_EMAIL, FEEDBACK_TEXT)
+        feedback_in_db = Feedback.query.get(send_feedback.id)
+        self.assertTrue(mock_send_to_trello_method.called)
+        self.assertEquals(feedback_in_db.trello_link, TRELLO_CARD_URL)

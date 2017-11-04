9f13905a88cae3d5e56c91487447fa207f7badc0
John Syomochkin <info@syomochkin.xyz>
2017-11-05 00:43:53 +0300

    #16 локальная нумерация для новых коммитов

--- application/models/Code.py
+++ application/models/Code.py
@@ -12,6 +12,7 @@ class Code(db.Model):
     :param content: содержимое исходного кода
     :param author: автор кода
     :param message: сообщение-описание редакции исходного кода
+    :param number: номер коммита, порядковый в рамках одного чата
     :param chat_link: ссылка на чат, к которому принадлежит данных код
     :param parent_link: ссылка на родителя, от которого образовался данных код
     :param create_time: время создания кода
@@ -22,6 +23,7 @@ class Code(db.Model):
     content = db.Column(db.Text)
     author = db.Column(db.Text)
     message = db.Column(db.Text)
+    number = db.Column(db.Integer, nullable=True)
     chat_link = db.Column(db.Integer, db.ForeignKey('chat.id'))
     parent_link = db.Column(db.Integer, db.ForeignKey('code.id'), nullable=True)
     create_time = db.Column(db.DateTime, nullable=False, default=db.func.now())
@@ -29,25 +31,28 @@ class Code(db.Model):
     chat = db.relationship('Chat', backref=db.backref('codes'))
     children = db.relationship('Code')
 
+    SYS_MESSAGE_ADD_TEMPLATE = u'Новое изменение {number} : {message}'
+
     def __init__(self, **params):
         self.content = params.get('content', "")
         self.author = User.get_login()
         self.chat_link = params.get('chat_link')
         self.parent_link = params.get('parent_link')
         self.message = MarkdownMixin.decode(params.get('message'))
+        self.number = Code.get_count_in_chat(params.get('chat_link')) + 1
 
     @staticmethod
-    def send(chat_id, text, parent, message):
+    def send(chat_id, code_text, parent, message):
         """Отправление кода на сервер
 
         :param chat_id: Номер чата
-        :param text: Код
+        :param code_text: Код
         :param parent: Место в дереве коммитов
         :param message: Комметарий к коду
         :return: Сообщение о коммите и номере кода
         """
         code_params = {
-            "content": text,
+            "content": code_text,
             "message": message,
             "chat_link": chat_id,
             "parent_link": parent
@@ -55,9 +60,11 @@ class Code(db.Model):
         code_to_send = Code(**code_params)
         db.session.add(code_to_send)
         db.session.commit()
-        code_id_in_chat = u"undefined"
-        text = u'Новое изменение {id} : {message}'
-        Message.send(chat_id, text.format(id=code_id_in_chat, message=message), 'sys')
+        message_text = Code.SYS_MESSAGE_ADD_TEMPLATE.format(
+            number=code_to_send.number,
+            message=message
+        )
+        Message.send(chat_id, message_text, Message.SYSTEM_TYPE)
         if app.config['SOCKET_MODE'] == 'True':
             socketio.emit('commit', room=str(chat_id), broadcast=True)
         return code_to_send.id
@@ -98,14 +105,22 @@ class Code(db.Model):
 
         :return: Вершина в дереве коммитов
         """
-        NODE_MARKUP = "<div class=\"commit_node circle unchosen\" data-id=\"{id}\">{id}</div>"
+        NODE_MARKUP = "<div class=\"commit_node circle unchosen\" data-id=\"{id}\">{number}</div>"
         node = {
             "text": {
                 "name": self.id,
                 "title": self.message
             },
-            "innerHTML": NODE_MARKUP.format(id=self.id)
+            "innerHTML": NODE_MARKUP.format(id=self.id, number=self.number)
         }
         children = [child.get_tree_node() for child in self.children]
         node["children"] = children
         return node
+
+    @staticmethod
+    def get_count_in_chat(chat_id):
+        """Получение количества коммитов в одном чате
+
+        :return: количества коммитов
+        """
+        return Code.query.filter_by(chat_link=chat_id).count()

4e9a3d38992d73eac319a548429fd83f1ec0cbd3
John Syomochkin <info@syomochkin.xyz>
2019-02-07 21:52:44 +0300

    #42 обработка ошибок 404 в рамках blueprint

--- application/api/exception.py
+++ application/api/exception.py
@@ -2,12 +2,13 @@
 
 """Обработки исключений/ошибок в запросах"""
 
-from flask import jsonify
+from flask import jsonify, abort
 from werkzeug.exceptions import BadRequest
 from application.api import api
 
 
 @api.errorhandler(BadRequest)
+@api.errorhandler(404)
 def handle_errors(error):
     """Обработка ошибок в запросах и возврат информации об ошибке в json формате"""
     response = jsonify(dict(
@@ -17,3 +18,8 @@ def handle_errors(error):
     ))
     response.status_code = error.code
     return response
+
+@api.route('/<path:invalid_path>')
+def handler_invalid_path(invalid_path):
+    """(hook) Вызов ошибки 404 в рамках blueprint"""
+    abort(404, 'URL /api/' + invalid_path + ' not found')
